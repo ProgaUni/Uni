faed3a55bdc98034713079f9e74ede9b61ee1fca
Vseva <103783951+Grindelwaldoff@users.noreply.github.com>
2023-12-09 16:42:03 +0300

    re

--- third_exam/first_folder☑️/first_algorithm.c
+++ third_exam/first_folder☑️/first_algorithm.c
@@ -4,6 +4,8 @@
 int* max_min_finder(Array array)
 {
     int* result = (int*)malloc(2 * sizeof(int));  // 0 элемент - min, 1 - max
+    result[0] = array.data[array.rows-1][array.cols-1];
+    result[1] = array.data[array.rows-1][array.cols-1];
     for (int rows = 1; rows < array.rows; rows++)
     {
         for (int cols = array.cols - (rows + 1); cols < array.cols; cols++)
